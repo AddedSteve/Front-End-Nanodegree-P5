b4f411fd0266acbb35d36b46ae908b65977e3f75
Steve O'Gallagher <ogallaghersteve@gmail.com>
2016-03-08 19:25:17 +0000

    Add ability for search menu to filter locations and markers to filter as well.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,23 @@
 var map;
 var markers = [];
+var clientID = "IQDJFYLUCO1UJ45322HIZZOVQWAQCEJ5NQHWP30X4IJ0RL5G";
+var clientSecret = "2DWL5WIZLGY44PN54M5CC0WUN0FD1OA1ULJYO0N322B5AXYV";
+
+var config = {
+    apiKey: 'IQDJFYLUCO1UJ45322HIZZOVQWAQCEJ5NQHWP30X4IJ0RL5G',
+    authUrl: 'https://foursquare.com/',
+    apiUrl: 'https://api.foursquare.com/'
+  };
+
+  $.getJSON('https://api.foursquare.com/v2/venues/search?ll=40.7,-74' +
+    '&query=mcdonalds&client_id=' + clientID + 
+    '&client_secret=' + clientSecret + '&v=20120101',
+
+    function(data) {
+        $.each(data.response.venues, function(i,venues){
+            console.log(venues.name);
+       });
+});
 
 function initMap () {
   map = new google.maps.Map(document.getElementById('map'), {
@@ -20,38 +38,43 @@ var Location = function (title, position, label) {
 };
 
 var Locations = [
-    new Location("All Star Lanes", [51.519879, -0.122544], 'A'),
-    new Location("Apple Store", [51.514270, -0.141909], 'B'),
-    new Location("The Cock Tavern", [51.516785, -0.141403], 'C'),
-    new Location("Foles", [51.514024, -0.129822], 'D'),
-    new Location("Bar", [51.515300, -0.129249], 'E')
+    new Location("All Star Lanes", [51.519879, -0.122544]),
+    new Location("Apple Store", [51.514270, -0.141909]),
+    new Location("The Cock Tavern", [51.516785, -0.141403]),
+    new Location("Foles", [51.514024, -0.129822]),
+    new Location("Bar", [51.515300, -0.129249])
 ];
+
 // our main view model
 var viewModel = {
 
     // map array of passed in Locations to an observableArray of Location objects
-    filteredLocations: ko.observableArray(Locations),
+    filteredLocations: ko.observableArray(Locations.slice()),
+
+    filteredMarkers: ko.observableArray(),
 
     query: ko.observable(''),
 
     search: function(value) {
         // remove all the current locations, which removes them from the view
         viewModel.filteredLocations.removeAll();
+        viewModel.setMapOnAll(null);
+        viewModel.filteredMarkers.removeAll();
 
         for(var x in Locations) {
           if(Locations[x].title().toLowerCase().indexOf(value.toLowerCase()) >= 0) {
             viewModel.filteredLocations.push(Locations[x]);
           }
         }
+        viewModel.applyMarkers();
       },
 
     applyMarkers: function(filteredLocations) {
 
-        filteredLocations.forEach(function (Location) {
+        viewModel.filteredLocations().forEach(function (Location) {
         // set even if value is the same, as subscribers are not notified in that case
             var marker = new google.maps.Marker({
                 position: {lat: Location.position()[0], lng: Location.position()[1]},
-                label: Location.label(),
                 map: map,
                 animation: google.maps.Animation.DROP,
                 title: 'Hello World!'
@@ -70,7 +93,16 @@ var viewModel = {
                 setTimeout(function(){ marker.setAnimation(null); }, 750);
               }
             });
+            viewModel.filteredMarkers().push(marker);
         })
+        viewModel.setMapOnAll(map);
+    },
+
+    setMapOnAll: function(map) {
+
+        for (var i = 0; i < viewModel.filteredMarkers().length; i++) {
+            viewModel.filteredMarkers()[i].setMap(map);
+        }
     },
 
     getWikipediaInformation: function(subject){
